6bef2c7409048d07bb293bbf037cdb49b668a093
alexislepresle <contact@alexislepresle.com>
2022-03-11 10:32:06 +0100

    add new testimonial + replace poujol logo

--- src/components/Sections/Testimonials.js
+++ src/components/Sections/Testimonials.js
@@ -1,20 +1,26 @@
 import React, { useState, useEffect } from 'react';
+import iconLP from "../../images/lepetitbiscuit-favicon.png"
+import iconDP from "../../images/domainepoujol-favicon.png"
+import iconEL from "../../images/escargotleandre-favicon.png"
 
 const data = [
     {
         name: "Aurélie",
         role: "Le Petit Biscuit Français",
-        testimonial: "Nous avons contacté Alexis pour développer un module de produit personnalisé. Alexis est un vrai développeur, qui ne sous-traite pas son travail et qui maîtrise son sujet. Les délais sont toujours respectés. Nous lui accordons depuis toute notre confiance. D'ailleurs, il s'occupe aujourd'hui de la maintenance du site à l'année. Vous l'aurez compris, nous recommandons fortement Alexis."
+        testimonial: "Nous avons contacté Alexis pour développer un module de produit personnalisé. Alexis est un vrai développeur, qui ne sous-traite pas son travail et qui maîtrise son sujet. Les délais sont toujours respectés. Nous lui accordons depuis toute notre confiance. D'ailleurs, il s'occupe aujourd'hui de la maintenance du site à l'année. Vous l'aurez compris, nous recommandons fortement Alexis.",
+        icon: iconLP
     },
     {
         name: "Emilie",
         role: "Domaine de Poujol",
-        testimonial: "Alexis est un super développeur Shopify! Nous avions initialement engagé un autre développeur pour notre site d’e-commerce. Le travail délivré était très peu qualitatif. Alexis a extrêmement bien rattrapé le coup! Il est super réactif, professionnel et impliqué. Je retravaillerai avec lui sur des futurs projets sans hésiter, et le recommande vivement :)"
+        testimonial: "Alexis est un super développeur Shopify! Nous avions initialement engagé un autre développeur pour notre site d’e-commerce. Le travail délivré était très peu qualitatif. Alexis a extrêmement bien rattrapé le coup! Il est super réactif, professionnel et impliqué. Je retravaillerai avec lui sur des futurs projets sans hésiter, et le recommande vivement :)",
+        icon: iconDP
     },
     {
         name: "Jacques",
         role: "Escargots Léandre",
-        testimonial: "Alexis est non seulement très compétent techniquement en e-commerce, FullStack... Mais il a aussi une très bonne compréhension des enjeux marketing, image, sales de ses clients. De plus, Alexis est une personne courtoise, organisée et force de propositions."
+        testimonial: "Alexis est non seulement très compétent techniquement en e-commerce. Mais il a aussi une très bonne compréhension des enjeux marketing, image, sales de ses clients. De plus, Alexis est une personne courtoise, organisée et force de propositions.",
+        icon: iconEL
     }
 ]
 
@@ -45,7 +51,7 @@ const Testimonials = () => {
                 </h2>
                 <div className="grid grid-cols-4 md:grid-cols-6 gap-4">
                     <div className="md:col-start-2 col-start-0 col-span-4">
-                        <div className="grid grid-cols-6 sm:h-64 md:h-40 h-80">
+                        <div className="grid grid-cols-6 sm:h-64 md:h-52 h-82">
                             <div className="md:col-span-1 col-span-0 absolute">
                                 <svg width="75" height="63" viewBox="0 0 75 63" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M0.5125 42.2C0.5125 33.4 3.3125 25.3333 8.9125 18C14.5125 10.6667 21.4458 4.79999 29.7125 0.399994L33.3125 6.79999C29.3125 9.73333 25.7125 12.8667 22.5125 16.2C19.3125 19.5333 16.5792 23.4667 14.3125 28L21.7125 34C24.7792 36.4 26.8458 38.8 27.9125 41.2C28.9792 43.6 29.5125 46.1333 29.5125 48.8C29.5125 52.9333 28.2458 56.2 25.7125 58.6C23.1792 61 19.9792 62.2 16.1125 62.2C13.5792 62.2 11.3125 61.7333 9.3125 60.8C7.3125 59.8667 5.5125 58.7333 3.9125 57.4C1.64583 52.6 0.5125 47.5333 0.5125 42.2ZM42.1125 42.2C42.1125 33.4 44.9125 25.3333 50.5125 18C56.1125 10.6667 63.0458 4.79999 71.3125 0.399994L74.9125 6.79999C70.9125 9.73333 67.3125 12.8667 64.1125 16.2C60.9125 19.5333 58.1792 23.4667 55.9125 28L63.3125 34C66.3792 36.4 68.4458 38.8 69.5125 41.2C70.5792 43.6 71.1125 46.1333 71.1125 48.8C71.1125 52.9333 69.8458 56.2 67.3125 58.6C64.7792 61 61.5792 62.2 57.7125 62.2C55.1792 62.2 52.9125 61.7333 50.9125 60.8C48.9125 59.8667 47.1125 58.7333 45.5125 57.4C43.2458 52.6 42.1125 47.5333 42.1125 42.2Z" fill="#B2BDD7" fill-opacity="0.29" />
@@ -104,19 +110,26 @@ const Testimonials = () => {
                         </div>
                     </div>
                 </div>
-                <div className="flex justify-center mt-10">
+                <div className="flex justify-center mt-10 flex-wrap">
                     {
                         data.map((testimonial, i) => {
                             return (
                                 <div
-                                    className={(i === currentTestimonial ? "px-6 rounded-full shadow-xl bg-gray-light " : " ") + "  px-5 py-2 cursor-pointer"}
+                                    className={(i === currentTestimonial ? "pl-2 pr-6 rounded-full shadow-xl bg-gray-light " : " ") + " mx-2 pl-2 pr-6 py-2 cursor-pointer"}
                                     onClick={() => setCurrentTestimonial(i)}
                                     onKeyDown={() => setCurrentTestimonial(i)}
                                     role="button"
                                     tabIndex={0}
                                 >
-                                    <p className="font-bold	">{testimonial.name}</p>
-                                    <p className="font-light font-gray-400 text-sm">{testimonial.role}</p>
+                                    <div className="flex items-center justify-center ">
+                                        <div className="bg-white rounded-full p-3 mr-2">
+                                            <img className="w-6 h-6" src={testimonial.icon} alt={testimonial.name} />
+                                        </div>
+                                        <div>
+                                            <p className="font-bold">{testimonial.name}</p>
+                                            <p className="font-light font-gray-400 text-sm opacity-50">{testimonial.role}</p>
+                                        </div>
+                                    </div>
                                 </div>
                             )
                         })
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,9 @@ module.exports = {
   theme: {
     textColor: theme => ({
       'black': '#494949',
-      'gray-400': 'rgba(73, 105, 153, 0.47)',
       'white': '#FFF',
       'indigo-600': '#4F46E5',
+      'gray-400': '#F9F9F9',
       'gray-700': '#7D8FAA'
     }),
     extend: {},
