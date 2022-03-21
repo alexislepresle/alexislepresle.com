f97bc46fbc46dc7964d93f9a0dc78a972266294d
alexislepresle <contact@alexislepresle.com>
2022-03-21 16:23:29 +0100

    some SEO update

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
       'white': '#FFF',
       'indigo-600': '#4F46E5',
       'gray-400': '#F9F9F9',
-      'gray-700': '#677891'
+      'gray-700': '#64758d'
     }),
     extend: {},
   },
